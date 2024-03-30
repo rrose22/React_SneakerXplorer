946ba0eafcf17ba035adb5b10bf371e14c6270b6
VirtAltoKohi <61061541+MRCGInteract@users.noreply.github.com>
2024-03-29 21:05:54 -0400

    Done some basic setup on the useAuthCnotext

--- client/src/Components/context/AuthProvider.js
+++ client/src/Components/context/AuthProvider.js
@@ -1,12 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext({});
 
+// Custom hook to access the authentication context
+export function useAuthContext() {
+    return useContext(authContext);
+}
+
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState({});
+    
+    const [authState, setAuthState] = useState({
+        user: null,
+        pwd: null,
+        isAuthenticated: false,
+        accessToken: null,
+        roles: null,
+    });
+
+    const setAuth = ({ user, pwd, roles, accessToken }) => {
+        setAuthState({
+            user,
+            pwd,
+            isAuthenticated: true,
+            accessToken,
+            roles,
+        })
+    }
+
+    const logout = () => {
+        setAuthState({
+            user: null,
+            pwd: null,
+            isAuthenticated: false,
+            accessToken: null,
+            roles: null,
+        })
+    }
 
     return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
+        <AuthContext.Provider value={{ ...authState, setAuth, logout }}>
             {children}
         </AuthContext.Provider>
     )
